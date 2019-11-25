3ddea1bce043307feb935caaf2d4059979d97c2c
Daniel Nass <cartmanmsi@gmail.com>
2019-11-25 14:44:40 -0300

    Updates

---
+++ precache-manifest.c25f1f1d2c8a0470e494cb995ac38b52.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fac4d43e09b66fbda51672e5314b02ae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "02dccd93a11c6edddadb",
+    "url": "/static/js/2.749aa5b6.chunk.js"
+  },
+  {
+    "revision": "1b1f97d26e58a9ff7745",
+    "url": "/static/js/main.b72f14f5.chunk.js"
+  },
+  {
+    "revision": "79e15aa0d2ebcc8d0172",
+    "url": "/static/js/runtime-main.b5357a1c.js"
+  }
+]);
